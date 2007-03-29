e592039f9936a2ef7faac371c6a6d32701cd73b6
Philipp von Weitershausen <philipp@weitershausen.de>
2007-03-29 22:16:16 +0000

    support for devmode

--- zope/paste/factory.py
+++ zope/paste/factory.py
@@ -1,7 +1,12 @@
 import zope.app.appsetup
 from zope.app.wsgi import WSGIPublisherApplication
 
-def zope_app_factory(global_conf, site_definition, file_storage):
-    zope.app.appsetup.config(site_definition)
+def zope_app_factory(global_conf, site_definition, file_storage,
+                     devmode='no'):
+    features = ()
+    if devmode.lower() in ('yes', 'true', 'on'):
+        features += ('devmode',)
+    zope.app.appsetup.config(site_definition, features)
+
     db = zope.app.appsetup.database(file_storage)
     return WSGIPublisherApplication(db)
