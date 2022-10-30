857c3073b2e4533f17645cb4e738b8c2a1cf5339
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-10-30 20:41:15 +0200

    Drop support for EOL Python 2.7

--- src/zope/paste/tests.py
+++ src/zope/paste/tests.py
@@ -24,12 +24,7 @@ import unittest
 
 import paste.deploy
 from waitress.server import WSGIServer
-
-try:
-    from urllib.request import urlopen
-except ImportError:
-    # PY2
-    from urllib2 import urlopen
+from urllib.request import urlopen
 
 # let every Python version run on its own port to allow parallel runs:
 PORT = 8765 + 10 * sys.version_info.major + sys.version_info.minor
